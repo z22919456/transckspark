9373c5d6ff8fa4b7b470a710f99c7ca9a6a2cdd5
z01241 <z01241@lawbroker.com.tw>
2022-09-24 03:56:40 +0800

    feat: add navigation

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import List from 'components/assets/nav/list.svg'
+import NavButton from './NavButton'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import classnames from 'classnames';
+
+type Props = { onClose: () => void }
+
+const NAV_LIST = [
+  { name: 'English', href: "/english" },
+  { name: '關於', href: "/about" },
+  { name: '最新消息', href: "/news" },
+  { name: '競圖', href: "/competition" },
+  { name: '評審', href: "/judge" },
+  { name: '入圍作品一覽', href: "/shortlist" },
+  { name: '想像中正紀念堂的100種方式', href: "/activity" },
+  { name: '常見問答', href: "/faq" },
+  { name: '競圖成果展覽', href: "/exhibition" },
+]
+
+function Navigation({ onClose }: Props) {
+  const route = useRouter()
+  const pathname = route.pathname
+  return (
+    <nav className="absolute top-0 left-0 flex w-1/2 h-screen max-h-screen p-5 overflow-hidden text-white bg-default">
+      <List className="mt-3" />
+      <div className="px-3">
+        <ul className="ml-3 space-y-3">
+          {NAV_LIST.map(list => (
+            <li className="h1">
+              <Link href={list.href}>
+                <a className={classnames('pb-1 transition-all ease-out delay-200 border-b-2 hover:border-white border-default', pathname == list.href && 'border-white')} onClick={onClose} >
+                  {list.name}
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <NavButton close className="absolute top-0 right-0" onClick={onClose} />
+    </nav>
+  )
+}
+
+export default Navigation
