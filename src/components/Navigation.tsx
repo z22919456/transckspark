656656935b22df9e4bde2b7b9ee5718b538dabd5
z01241 <z01241@lawbroker.com.tw>
2022-09-25 23:38:17 +0800

    feat(nav): add  connection button

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,6 +4,8 @@ import NavButton from './NavButton'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import classnames from 'classnames';
+import YoutubeIcon from 'components/assets/footer/yt.svg'
+import FacebookIcon from 'components/assets/footer/fb.svg'
 
 type Props = { onClose: () => void }
 
@@ -25,8 +27,8 @@ function Navigation({ onClose }: Props) {
   return (
     <nav className="fixed top-0 left-0 flex w-1/2 h-screen max-h-screen p-5 overflow-hidden text-white bg-default">
       <List className="mt-3" />
-      <div className="px-3">
-        <ul className="ml-3 space-y-3">
+      <div className="relative h-full px-3 ml-3 lg:ml-14 max-h-[720px]">
+        <ul className="space-y-3">
           {NAV_LIST.map(list => (
             <li className="h1">
               <Link href={list.href}>
@@ -37,6 +39,13 @@ function Navigation({ onClose }: Props) {
             </li>
           ))}
         </ul>
+        <div className="absolute bottom-0 left-0 flex flex-wrap items-center space-x-5">
+          <FacebookIcon className="w-6 h-6 fill-white" />
+          <YoutubeIcon className="w-6 h-6 fill-white" />
+          <a className="small" href="email://info@vipassanacreative.com">
+            info@vipassanacreative.com
+          </a>
+        </div>
       </div>
       <NavButton close className="absolute top-0 right-0" onClick={onClose} />
     </nav>
