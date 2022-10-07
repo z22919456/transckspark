aba2ecfa7edd30b9fae7f6e5fba6509b2eaed38f
z01241 <z01241@lawbroker.com.tw>
2022-10-07 19:35:54 +0800

    feat(faq): update question

--- src/data/faqData.ts
+++ src/data/faqData.ts
@@ -2,37 +2,37 @@ export const faqData1 = [
   {
     id: 1,
     question: '完成報名是甚麼時候？報名有截止日嗎？',
-    answer: '',
+    answer: '本次競圖需於初選交件截止日2022.11.10前完成報名。完成報名後，方可取得相關圖資，以及優先參加民眾參與活動，以便完成評比標準之內容，請有志參加者儘早完成報名。',
   },
   {
     id: 2,
     question: '請問我提交報名表單後，會收到 「 報名成功確認信 」 嗎？',
-    answer: '',
+    answer: '主辦單位不會個別寄出報名成功確認信，但會在確認報名表單內容無誤後，寄發基地圖資使用切結書給報名者。',
   },
   {
     id: 3,
     question: '報名後主辦方會提供基地相關資料或圖檔嗎？',
-    answer: '',
+    answer: '會。在您填寫報名表單後，主辦單位將寄送一份圖檔使用切結書，報名者簽立同意書後，將統一寄發基本圖資。',
   },
   {
     id: 4,
     question: '在報名時尚未有明確的設計方向，就先決定 「 團隊名稱 」，未來是否還可以更改團隊名稱以切合設計理念？',
-    answer: '',
+    answer: '可以，只要在初選交件截止日前，主動來信至info@vipassanacreative.com要求修改即可。',
   },
   {
     id: 5,
     question: '請問提交表單後，團隊成員還能變動（增加或是退出）嗎？',
-    answer: '',
+    answer: '可以，只要在初選交件截止日前，主動來信至info@vipassanacreative.com要求修改團隊資訊，並附上新成員簽署的切結書即可。',
   },
   {
     id: 6,
     question: '本次競圖是否允許重複投稿，一人可同時參與不同組別嗎？',
-    answer: '',
+    answer: '不可以。本次競圖每位參賽者僅限報名一個組別，且不可重複投稿。',
   },
   {
     id: 7,
     question: '本次競圖範圍？',
-    answer: '',
+    answer: '本次競圖以中山南路、信義路、杭州南路、愛國東路圍合之中正紀念堂園區全區為重點設計範圍，參賽者需回應評比標準提到的內容。',
   },
 ];
 
@@ -40,6 +40,6 @@ export const faqData2 = [
   {
     id: 1,
     question: '中正要討論講座、工作坊會錄影並會放上網站嗎？',
-    answer: '這是一個假設的解答，中正要討論講座、工作坊會錄影會放上網站，請到想像中正紀念堂100種方式。',
+    answer: '會，請上競圖執行小組<a href="https://www.youtube.com/playlist?list=PLZJKcj3-88_jHVt2MOGOuW8zSzpLb4r6r" class="underline decoration-1" rel="noreferrer noopener" target="_blank">游觀創意YouTube頻道觀賞<a>',
   },
 ];
--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -17,7 +17,7 @@ function Faq() {
       <div className="px-5">
         <div className="mb-20">
           <h1 className="mb-5 text-base text-center">競圖報名相關問題</h1>
-          <Accordion type="single" >
+          <Accordion type="single" collapsible>
             {faqData1.map((faq) => (
               <AccordionItem value={faq.id.toString()} key={faq.id}>
                 <AccordionTrigger>
@@ -33,14 +33,14 @@ function Faq() {
 
         <div>
           <h1 className="mb-5 text-base text-center">想像中正紀念堂的100種方式系列活動相關問題</h1>
-          <Accordion type="single" >
+          <Accordion type="single" collapsible>
             {faqData2.map((faq) => (
               <AccordionItem value={faq.id.toString()} key={faq.id}>
                 <AccordionTrigger>
                   {faq.question}
                 </AccordionTrigger>
                 <AccordionContent>
-                  {faq.answer}
+                  <p dangerouslySetInnerHTML={{ __html: faq.answer }}></p>
                 </AccordionContent>
               </AccordionItem>
             ))}
