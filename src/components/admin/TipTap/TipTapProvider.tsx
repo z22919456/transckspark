560bf8d2986d150fc1d40a4dbecf9a181430b6a3
z01241 <z01241@lawbroker.com.tw>
2022-10-24 17:10:11 +0800

    feat(delete file): remove unused file

--- src/components/admin/TipTap/TipTapProvider.tsx
+++
@@ -1,8 +0,0 @@
-import { Editor } from '@tiptap/react';
-import { createContext, useContext } from 'react';
-
-const TipTabContext = createContext<Editor>({} as Editor);
-
-export const TipTabProvider = TipTabContext.Provider;
-
-export const useEditorContext = () => useContext(TipTabContext);
