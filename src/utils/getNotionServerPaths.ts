c57aaccaaaf3c30d25a0760efaf64f5dc1eea061
z01241 <z01241@lawbroker.com.tw>
2022-10-25 22:25:46 +0800

    fix(getNotionServerPaths): should return a function

--- src/utils/getNotionServerPaths.ts
+++ src/utils/getNotionServerPaths.ts
@@ -1,6 +1,6 @@
 import { getPageList } from 'lib/notion';
 
-const getNotionServerPaths = async (dbId: string) => {
+const getNotionServerPaths = (dbId: string) => async () => {
   try {
     const pageList = await getPageList(dbId);
     const ids = await pageList.map((page) => ({ params: page.id }));
