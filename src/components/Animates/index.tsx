ec55b58a510460ebc7122f7b1ae7119596a60112
z01241 <z01241@lawbroker.com.tw>
2022-10-26 23:46:24 +0800

    fix(Animate): restore random number

--- src/components/Animates/index.tsx
+++ src/components/Animates/index.tsx
@@ -9,11 +9,8 @@ function Animates() {
   useEffect(() => {
     setHasMounted(true);
   }, []);
-  // TODO: create loading animation
   if (!hasMounted) return <></>;
-  // const random = Math.floor(Math.random() * 3);
-  const random = 1;
-  console.log(random);
+  const random = Math.floor(Math.random() * 3);
   if (random === 0) return <AnimateIframe />;
   if (random === 1) {
     return (
