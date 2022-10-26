a589c73f3619d4261489f7045a039fa30c1d9b45
z01241 <z01241@lawbroker.com.tw>
2022-10-26 21:04:59 +0800

    fix(ShareButton): adjust share button hover state

--- src/components/SocialShareButton.tsx
+++ src/components/SocialShareButton.tsx
@@ -19,7 +19,7 @@ function SocialShareButton({ left = false }: Props) {
 
         <Link href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURI(host + asPath)}`} target="_blank">
           <a className="flex flex-col items-center text-center group" rel="noreferrer noopener" target="_blank">
-            <div className="flex items-center justify-center w-10 h-10 my-auto border border-black rounded-full hover:bg-primary">
+            <div className="flex items-center justify-center w-10 h-10 my-auto border border-black rounded-full group-hover:bg-primary">
               <FontAwesomeIcon icon={faFacebook} />
             </div>
             <span className="small text-default/70 group-hover:text-primary">Facebook</span>
@@ -27,7 +27,7 @@ function SocialShareButton({ left = false }: Props) {
         </Link>
         <Link href={`https://twitter.com/intent/tweet?url=${encodeURI(host + asPath)}`} >
           <a className="flex flex-col items-center text-center group" rel="noreferrer noopener" target="_blank">
-            <div className="flex items-center justify-center w-10 h-10 my-auto border border-black rounded-full hover:bg-primary">
+            <div className="flex items-center justify-center w-10 h-10 my-auto border border-black rounded-full group-hover:bg-primary">
               <FontAwesomeIcon icon={faTwitter} />
             </div>
             <span className="small text-default/70 group-hover:text-primary">Twitter</span>
