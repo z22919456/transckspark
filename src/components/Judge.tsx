37394ab580b93a65d4d9fb0c9536b326b926e044
z01241 <z01241@lawbroker.com.tw>
2022-10-26 20:48:32 +0800

    fix(judge): swap judge tag

--- src/components/Judge.tsx
+++ src/components/Judge.tsx
@@ -10,7 +10,7 @@ function Judge({
   return (
     <div className="w-full py-5 md:p-5 border-t md:w-1/2 md:odd:border-r border-default relative before:content-none md:odd:before:content-[''] before:absolute before:h-10 before:w-10 before:top-0 before:right-0 before:translate-x-1/2 before:-translate-y-1/2 before:bg-white before:z-[1]" id={`${final ? 'final' : 'first'}-${id}`}>
       <div className="flex justify-between text-xs">
-        <p>{final ? '初選評審' : '決選評審'}</p>
+        <p>{final ? '決選評審' : '初選評審'}</p>
         <p>{engName}</p>
       </div>
       <h2 className="py-10 text-lg text-center">{name}</h2>
