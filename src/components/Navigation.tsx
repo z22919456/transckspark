0a266210b70b29ed5f82c9162d433e96acd74aae
z01241 <z01241@lawbroker.com.tw>
2022-10-31 21:47:15 +0800

    fix(judge, navbar): remove some text and add nav english name

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,6 +2,7 @@ import classnames from 'classnames';
 import FacebookIcon from 'components/assets/footer/fb.svg';
 import YoutubeIcon from 'components/assets/footer/yt.svg';
 import List from 'components/assets/nav/list.svg';
+import { lstat } from 'fs';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
@@ -11,14 +12,16 @@ import NavButton from './NavButton';
 type Props = { onClose: () => void };
 
 const NAV_LIST = [
-  { name: 'English', href: '/en' },
-  { name: '首都之心 城中轉正', href: '/' },
-  { name: '最新消息', href: '/news' },
-  { name: '競圖活動說明', href: '/competition' },
-  { name: '評審委員', href: '/judges' },
-  { name: '第一階段入圍作品一覽 (2022.12.1揭曉)', href: '/competition_finalists', inactive: true },
-  { name: '想像中正紀念堂的100種方式', href: '/public_participation' },
-  { name: '常見問答', href: '/faq' },
+  { name: 'English', subName: '', href: '/en' },
+  { name: '關於', subName: 'About the Project', href: '/' },
+  { name: '最新消息', subName: 'News', href: '/news' },
+  { name: '競圖活動說明', subName: 'Competition', href: '/competition' },
+  { name: '評審委員', subName: 'The Jury', href: '/judges' },
+  {
+    name: '第一階段入圍作品一覽', subName: 'Competition Finalist (2022.12.1揭曉)', href: '/competition_finalists', inactive: true,
+  },
+  { name: '想像中正紀念堂的100種方式', subName: 'Public Participation', href: '/public_participation' },
+  { name: '常見問答', subName: 'FAQ', href: '/faq' },
   // { name: '競圖成果展覽', href: '/exhibition' },
 ];
 
@@ -27,19 +30,19 @@ function Navigation({ onClose }: Props) {
   const { pathname } = route;
   return (
     <nav className="fixed top-0 left-0 z-50 flex w-screen h-screen max-h-screen p-5 overflow-hidden text-white md:w-1/2 bg-default">
-      <List className="w-16 mt-3" />
-      <div className="relative h-full px-3 ml-3 lg:ml-14 max-h-[720px] w-full">
+      <List className="flex-shrink-0 w-16 mt-3" />
+      <div className="relative h-full px-3 ml-0 lg:ml-5 max-h-[720px] w-full">
         <ul className="space-y-3">
           {NAV_LIST.map((list) => (
             <li className="h3" key={list.href}>
               {list.inactive ? (
                 <p className={classnames('pb-1 transition-all ease-out delay-200 border-b-2 text-gray-500 border-default', pathname === list.href && 'border-white')} onClick={onClose} >
-                  {list.name}
+                  {list.name} <span className="text-xs font-normal">{list.subName}</span>
                 </p>
               ) : (
                 <Link href={list.href}>
                   <a className={classnames('pb-1 transition-all ease-out delay-200 border-b-2 hover:translate-x-5 hover:border-white border-default', pathname === list.href && 'border-white')} onClick={onClose} >
-                    {list.name}
+                    {list.name} <span className="text-xs font-normal">{list.subName}</span>
                   </a>
                 </Link>
               )
