8990b97ec9819725fbbeeb609228c31c209dfed2
z01241 <z01241@lawbroker.com.tw>
2022-10-31 09:30:24 +0800

    feat(SEO): update tag

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -1,6 +1,7 @@
 import { NextSeo } from 'next-seo';
 import Head from 'next/head';
 import React from 'react';
+import { useRouter } from 'next/router';
 
 type Props = {
   title?: string
@@ -10,19 +11,19 @@ type Props = {
 function SEO({ title, description }: Props) {
   const allTitle = title ? `${title} | 中正紀念堂園區新願景概念競圖` : '中正紀念堂園區新願景概念競圖';
   const allDescription = description || '首都之心，城中轉正：中正紀念堂園區新願景概念競圖，是民間發起的紙上概念競圖，由台灣現代建築學會、中華民國都市設計學會、中華民國景觀學會國內三大空間規劃設計專業學會共同發起。攸關台灣當代城市生活的形塑和社會價值的再造，也是讓生活塑造城市、強化參與的難得機會。';
+  const { asPath } = useRouter();
   return (
     <NextSeo
       title={allTitle}
       description={allDescription}
       openGraph={{
+        url: `${process.env.NEXT_PUBLIC_HOST}/${asPath}`,
         type: 'website',
         title: allTitle,
         description: allDescription,
         images: [
           {
             url: `${process.env.NEXT_PUBLIC_HOST}/metatag.png`,
-            alt: 'Og Image Alt',
-            type: 'image/png',
           },
         ],
       }}
