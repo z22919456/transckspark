d679420ae3e408f065d972784e03754e9a6a0aff
z01241 <z01241@lawbroker.com.tw>
2022-11-01 23:52:10 +0800

    feat(Image): add placeholder

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -25,21 +25,21 @@ function LandingPage() {
   return (
     <div className={classnames('fixed left-0 z-50 w-screen h-screen p-10 bg-default duration-300 transition-all ease', !show && '-translate-y-full opacity-0')}>
       <div className="relative hidden w-full h-1/2 sm:block">
-        <Image priority src={eyesMd} layout="fill" objectPosition="top" objectFit='contain' alt="" ></Image>
+        <Image placeholder="blur" blurDataURL="/image_loading.jpg" priority src={eyesMd} layout="fill" objectPosition="top" objectFit='contain' alt="" ></Image>
       </div>
       <div className="block w-full pt-16 sm:hidden h-3/4">
         <div className="relative w-full h-full">
-          <Image priority src={eyes} layout="fill" objectPosition="top" objectFit='contain' alt="" ></Image>
+          <Image placeholder="blur" blurDataURL="/image_loading.jpg" priority src={eyes} layout="fill" objectPosition="top" objectFit='contain' alt="" ></Image>
         </div>
       </div>
       <div className="relative hidden w-full h-1/2 md:block">
-        <Image priority className="" src={infoMd} layout="fill" objectPosition="bottom" objectFit='contain' alt="" ></Image>
+        <Image placeholder="blur" blurDataURL="/image_loading.jpg" priority className="" src={infoMd} layout="fill" objectPosition="bottom" objectFit='contain' alt="" ></Image>
       </div>
       <div className="relative hidden w-full h-1/2 sm:block md:hidden">
-        <Image priority className="hidden md:block lg:hidden" src={infoSm} layout="fill" objectPosition="bottom" objectFit='contain' alt="" ></Image>
+        <Image placeholder="blur" blurDataURL="/image_loading.jpg" priority className="hidden md:block lg:hidden" src={infoSm} layout="fill" objectPosition="bottom" objectFit='contain' alt="" ></Image>
       </div>
       <div className="relative block w-full h-1/4 sm:hidden">
-        <Image priority className="hidden md:block lg:hidden" src={info} layout="fill" objectPosition="bottom" objectFit='contain' alt="" ></Image>
+        <Image placeholder="blur" blurDataURL="/image_loading.jpg" priority className="hidden md:block lg:hidden" src={info} layout="fill" objectPosition="bottom" objectFit='contain' alt="" ></Image>
       </div>
     </div>
   );
