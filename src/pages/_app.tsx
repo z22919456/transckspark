d0d2b46086e8d1a4f7728c620e3bf096f470862e
z01241 <z01241@lawbroker.com.tw>
2022-11-03 22:13:52 +0800

    feat(GTM): setting gtm

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import React, { ReactElement, ReactNode } from 'react';
 
 import type { AppProps } from 'next/app';
 import { NextPage } from 'next';
+import Script from 'next/script';
 
 type NextPageWithLayout<P = unknown, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement, pageProps: P) => ReactNode
@@ -19,6 +20,15 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || withDefaultLayout;
   return (
     <>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-NFZSSFN');
+      `}
+      </Script>
       {getLayout(<Component {...pageProps} />, pageProps)}
     </>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,6 +14,11 @@ function Document() {
       <body>
         <Main />
         <NextScript />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NFZSSFN" height="0" width="0" style="display: none; visibility: hidden;" />',
+          }}
+        />
       </body>
     </Html>
   );
