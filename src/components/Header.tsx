7a25ab0cdfa97fac00c08e8a7b95d17f3ef42fe8
z01241 <z01241@lawbroker.com.tw>
2022-11-09 13:30:35 +0800

    feat(title): add title for countdown

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,7 +25,8 @@ function Header({ className }: Props) {
     <div className={classnames('relative md:z-10 -z-10  w-full h-20 px-5 bg-gradient-to-b from-primary to-white transition-all duration-200 -mb-20 md:mb-0', className)}>
       <div className="items-center justify-end hidden w-full h-full md:flex">
         <TitleSvg className="absolute -translate-y-1/2 top-1/2 left-5" />
-        <div className="mb-1 text-right text-black/50 lg:flex lg:space-x-5">
+        <div className="flex flex-col mb-1 text-right text-black/50 lg:flex lg:space-x-5">
+          <p className="mt-1 text-sm">初審入圍名單公布倒數</p>
           <p>{duration.day} Days {duration.time}</p>
         </div>
       </div>
