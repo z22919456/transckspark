2d5412e60f2b6eedda1318b26eeed525a2b2ef01
z01241 <z01241@lawbroker.com.tw>
2022-11-29 23:38:01 +0800

    update counter

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,10 +9,22 @@ type Props = {
 
 const FIRST_TIME = '2022-12-01T12:00:00.000+08:00';
 const SECOND_TIME = '2023-02-15T12:00:00.000+08:00';
+const THIRD_TIME = '2023-03-10T12:00:00.000+08:00';
+
+const TIME_POINTS = [FIRST_TIME, SECOND_TIME, THIRD_TIME];
+
+const TITLE = ['初審入圍名單公布倒數', '決選交件截止日倒數', '決選優勝者公布'];
 
 function Header({ className }: Props) {
   const now = dayjs();
-  const to = useMemo(() => dayjs(dayjs().isBefore(dayjs(FIRST_TIME)) ? FIRST_TIME : SECOND_TIME), []);
+  const stage = useMemo(() => {
+    if (dayjs().isBefore(dayjs(FIRST_TIME))) return 0;
+    if (dayjs().isBefore(dayjs(SECOND_TIME))) return 1;
+    if (dayjs().isBefore(dayjs(THIRD_TIME))) return 2;
+    return 3;
+  }, []);
+  const title = useMemo(() => TITLE[stage], [stage]);
+  const to = useMemo(() => dayjs(TIME_POINTS[stage]), [stage]);
   const [duration, setDuration] = useState({ day: 0, time: '00:00:00' });
 
   useEffect(() => {
@@ -29,7 +41,7 @@ function Header({ className }: Props) {
       <div className="items-center justify-end hidden w-full h-full md:flex">
         <TitleSvg className="absolute -translate-y-1/2 top-1/2 left-5" />
         <div className="flex flex-col mb-1 text-right text-black/50 lg:flex lg:space-x-5">
-          <p className="mt-1 text-sm">初審入圍名單公布倒數</p>
+          <p className="mt-1 text-sm">{title}</p>
           <p>{duration.day} Days {duration.time}</p>
         </div>
       </div>
