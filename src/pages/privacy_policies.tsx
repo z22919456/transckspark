722fc1ce0ec853617872d96eb44be76e99d0f0ef
z01241 <z01241@lawbroker.com.tw>
2022-11-30 00:08:41 +0800

    update private_policy page

---
+++ src/pages/privacy_policies.tsx
@@ -0,0 +1,38 @@
+import 'react-notion-x/src/styles.css';
+
+import { NotionRenderer } from 'react-notion-x';
+import React, { ReactNode } from 'react';
+import { NotionAPI } from 'notion-client';
+import { ExtendedRecordMap } from 'notion-types';
+import Image from 'next/image';
+
+type Props = {
+  blocks: ExtendedRecordMap
+};
+
+function PrivacyPolicy({ blocks }: Props) {
+  return <NotionRenderer components={{ nextImage: Image }} recordMap={blocks} />;
+}
+
+PrivacyPolicy.getLayout = (children: ReactNode) => <main className="p-5">{children}</main>;
+
+export const getStaticProps = async () => {
+  try {
+    const notion = new NotionAPI();
+    const page = await notion.getPage('cee103878d234b649b3fcbe3bff6702f');
+    // console.log(page);
+    // const response = await axios.get('https://notion-api.splitbee.io/v1/page/cee103878d234b649b3fcbe3bff6702f');
+    return {
+      props: {
+        blocks: page,
+      },
+      revalidate: 10,
+    };
+  } catch {
+    return {
+      notFound: true,
+    };
+  }
+};
+
+export default PrivacyPolicy;
