9f1e46b87bdb397dec989ef4247b719efabdf3c0
z01241 <z01241@lawbroker.com.tw>
2022-12-02 13:33:36 +0800

    typo

--- src/pages/competition_shortlists/index.tsx
+++ src/pages/competition_shortlists/index.tsx
@@ -21,7 +21,7 @@ type Props = {
 function CompetitionShortlists({ pageList }: Props) {
   return (
     <>
-      <SEO title="競圖活動說明  Competition" description="首都之心 城中轉正：中正紀念堂園區新願景概念競圖 第一階段入圍作品。" />
+      <SEO title="第一階段入圍作品一覽  Competition Shortlists" description="首都之心 城中轉正：中正紀念堂園區新願景概念競圖 第一階段入圍作品。" />
       <div className="shortlist">
         <Header />
         <div className="mx-5">
