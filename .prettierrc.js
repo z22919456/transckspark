354edffe86c99c9d4fad27f41acde45453bd1015
z01241 <z01241@lawbroker.com.tw>
2023-10-10 16:25:56 +0800

    feat: add new exhibition

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: "es5",
+  singleQuote: true,
+  printWidth: 80,
+  semi: true,
+  plugins: ["prettier-plugin-tailwindcss"],
+  bracketSameLine: false,
+  bracketSpacing: true,
+  useTab: true,
+  arrowParens: "always",
+};
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import dayjs from 'dayjs';
 import React, { useEffect, useMemo, useState } from 'react';
 
 type Props = {
-  className?: string
+  className?: string;
 };
 
 const FIRST_TIME = '2022-12-01T12:00:00.000+08:00';
@@ -12,26 +12,39 @@ const SECOND_TIME = '2023-02-15T12:00:00.000+08:00';
 const THIRD_TIME = '2023-03-10T12:00:00.000+08:00';
 const FOUR_TIME = '2023-08-25T10:30:00.000+08:00';
 const FIVE_TIME = '2023-09-10T18:00:00.000+08:00';
+const SIX_TIME = '2023-10-10T09:00:00.000+08:00';
+const SEVEN_TIME = '2023-12-17T18:00:00.000+08:00';
 
-const TIME_POINTS = [FIRST_TIME, SECOND_TIME, THIRD_TIME, FOUR_TIME, FIVE_TIME];
+const TIME_POINTS = [
+  FIRST_TIME,
+  SECOND_TIME,
+  THIRD_TIME,
+  FOUR_TIME,
+  FIVE_TIME,
+  SIX_TIME,
+  SEVEN_TIME,
+];
 
-const TITLE = ['初審入圍名單公布倒數', '決選交件截止日倒數', '決選優勝者公布', '成果展覽開幕日', '成果展覽展覽中'];
+const TITLE = [
+  '初審入圍名單公布倒數',
+  '決選交件截止日倒數',
+  '決選優勝者公布',
+  '成果展覽開幕日',
+  '成果展覽展覽中',
+  '成果展覽開幕日',
+  '成果展覽展覽中',
+];
 
 function Header({ className }: Props) {
   const now = dayjs();
-  const stage = useMemo(() => {
-    if (dayjs().isBefore(dayjs(FIRST_TIME))) return 0;
-    if (dayjs().isBefore(dayjs(SECOND_TIME))) return 1;
-    if (dayjs().isBefore(dayjs(THIRD_TIME))) return 2;
-    if (dayjs().isBefore(dayjs(FOUR_TIME))) return 3;
-    if (dayjs().isBefore(dayjs(FIVE_TIME))) return 4;
-    return 5;
-  }, []);
-  const title = useMemo(() => TITLE[stage], [stage]);
-  const to = useMemo(() => dayjs(TIME_POINTS[stage]), [stage]);
+  const stageIndex = useMemo(() => TIME_POINTS.findIndex((time) => dayjs().isBefore(dayjs(time))), []);
+
+  const title = useMemo(() => TITLE[stageIndex], [stageIndex]);
+  const to = useMemo(() => dayjs(TIME_POINTS[stageIndex]), [stageIndex]);
   const [duration, setDuration] = useState({ day: 0, time: '00:00:00' });
 
   useEffect(() => {
+    if (stageIndex === -1) return () => {};
     const timer = setInterval(() => {
       const day = to.diff(now, 'd');
       const time = dayjs(to.diff(now)).subtract(8, 'h').format('HH:mm:ss');
@@ -41,13 +54,30 @@ function Header({ className }: Props) {
   }, [now, to]);
 
   return (
-    <div className={classnames('relative md:z-10 -z-10  w-full h-20 px-5 bg-gradient-to-b from-primary to-white transition-all duration-200 -mb-20 md:mb-0', className)}>
+    <div
+      className={classnames(
+        'relative md:z-10 -z-10  w-full h-20 px-5 bg-gradient-to-b from-primary to-white transition-all duration-200 -mb-20 md:mb-0',
+        className,
+      )}
+    >
       <div className="items-center justify-end hidden w-full h-full md:flex">
         <TitleSvg className="absolute -translate-y-1/2 top-1/2 left-5" />
-        <div className="flex flex-col mb-1 text-right text-black/50 lg:flex lg:space-x-5">
-          <p className="mt-1 text-sm">{title}</p>
-          <p>{duration.day} Days {duration.time}</p>
-        </div>
+        {stageIndex !== -1 ? (
+          <div className="flex flex-col mb-1 text-right text-black/50 lg:flex lg:space-x-5">
+            <p className="mt-1 text-sm">{title}</p>
+
+            <p>
+              {duration.day} Days {duration.time}
+            </p>
+          </div>
+        ) : (
+          <div className="flex flex-col mb-1 text-right text-black/50 lg:flex lg:space-x-5">
+            <p className="mt-1 text-sm">{title}</p>
+            <p>
+              {dayjs().format('YYYY/MM/DD')}
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
