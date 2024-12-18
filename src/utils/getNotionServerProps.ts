68e3e8d303ce1d310385cf4e363b27cf9723ae4c
z01241 <z01241@lawbroker.com.tw>
2024-12-18 22:18:15 +0800

    fix: cover missing

--- src/utils/getNotionServerProps.ts
+++ src/utils/getNotionServerProps.ts
@@ -14,6 +14,8 @@ const getPageServerSideProps = (dbId: string) => async (context: GetStaticPropsC
 
     const page = await getPage(id);
 
+    console.log(page);
+
     const signedUrlKeys = Object.keys(page.signed_urls);
 
     const newSignedUrl: ExtendedRecordMap['signed_urls'] = {};
@@ -21,6 +23,16 @@ const getPageServerSideProps = (dbId: string) => async (context: GetStaticPropsC
     await Promise.all(signedUrlKeys.map(async (key) => {
       newSignedUrl[key] = await uploadImage(page.signed_urls[key], 5);
     }));
+
+    // 如果newSignedUrl是空的，代表沒有圖片
+    if (Object.keys(newSignedUrl).length === 0) {
+      const key = pageInformation?.id || '';
+      const coverUrl = pageInformation?.cover.url || '';
+      if (key && coverUrl) {
+        newSignedUrl[key] = await uploadImage(coverUrl, 5);
+        signedUrlKeys.push(key);
+      }
+    }
 
     // https://file.notion.so/f/s/a1bf65e2-567a-4508-93f3-85b0bae5cbf0/%E6%B4%BB%E5%8B%95%E5%B0%81%E9%9D%A2for_new_site(16x9)-05.jpg?id=0d34216c-b13f-47fa-a8e7-b0ae0a10b0bf&table=block&spaceId=b0498f07-0602-4afe-a9a7-0b552608003f&expirationTimestamp=1683640194876&signature=Zz8BDooXt1G9kK-VwKAGGtkG_TbZyyQ2BsdYELbWDF8
 
